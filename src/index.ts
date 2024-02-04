9f1a532adfb0fae89de01bee829769d131075795
pankaj-2503 <pankajpj3103@gmail.com>
2024-02-04 20:50:54 +0530

    orm

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@prisma/client";
+const prisma= new PrismaClient();
+
+async function insertUser(username: string, password: string, firstName: string, lastName: string) {
+    const res = await prisma.user.create({
+      data: {
+          email:username,
+          password,
+          firstName,
+          lastName
+      }
+    })
+    console.log(res);
+  }
+  
+  insertUser("admin1", "123456", "pankaj", "singh")
